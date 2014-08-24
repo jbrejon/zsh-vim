1e5bb8d4795ac9b07fa2943e197dbe1fb2dd35f9
herve <herve@debian-jb>
2014-08-24 12:21:14 +0200

    new install

--- install.sh
+++ install.sh
@@ -5,14 +5,18 @@ mv ~/.zshrc ~/backup_zshv
 mv ~/.dircolors ~/backup_zshv
 mv ~/.indent.pro ~/backup_zshv
 mv ~/.oh-my-zsh ~/backup_zshv
+echo "mv"
 
 cp -r .oh-my-zsh ~/
 cp .zshrc ~/
 cp -r .vim* ~/
 cp .dircolors ~/
 cp .indent.pro ~/
+echo "cp"
 
 ./gnome-terminal-colors-solarized/set_alternative_dark.sh
+echo "bg set"
 chsh -s $(which zsh)
+echo "zsh set"
 
 echo "done"
